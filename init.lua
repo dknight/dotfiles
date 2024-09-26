7a70094637ed0c52b49d4621d2ef386a9d593625
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-09-27 00:57:12 +0300

    chore up

--- init.lua
+++ init.lua
@@ -169,6 +169,34 @@ local plugins = {
 	-- text
 	"rhysd/vim-grammarous",
 	-- "dpelle/vim-LanguageTool"
+	-- {
+	-- 	"Bryley/neoai.nvim",
+	-- 	dependencies = {
+	-- 		"MunifTanjim/nui.nvim",
+	-- 	},
+	-- 	cmd = {
+	-- 		"NeoAI",
+	-- 		"NeoAIOpen",
+	-- 		"NeoAIClose",
+	-- 		"NeoAIToggle",
+	-- 		"NeoAIContext",
+	-- 		"NeoAIContextOpen",
+	-- 		"NeoAIContextClose",
+	-- 		"NeoAIInject",
+	-- 		"NeoAIInjectCode",
+	-- 		"NeoAIInjectContext",
+	-- 		"NeoAIInjectContextCode",
+	-- 	},
+	-- 	keys = {
+	-- 		{ "<leader>as", desc = "summarize text" },
+	-- 		{ "<leader>ag", desc = "generate git message" },
+	-- 	},
+	-- 	config = function()
+	-- 		require("neoai").setup({
+	-- 			-- Options go here
+	-- 		})
+	-- 	end,
+	-- },
 }
 
 -- languagetool
