faa8af800086037e8dda39bae38aac626aa4e70a
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2025-04-07 07:14:18 +0300

    change lazy

--- init.lua
+++ init.lua
@@ -1,8 +1,36 @@
+-- Lazy setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- local uname = vim.fn.system("uname -s")
 local homedir = vim.fn.expand("$HOME")
 local stylua_cfg = homedir .. "/lab/dotfiles/.stylua.toml"
 
--- local uname = vim.fn.system("uname -s")
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
 vim.opt.exrc = false
 vim.opt.secure = true
 vim.opt.number = true
@@ -109,18 +137,6 @@ vim.keymap.set("n", "<c-l>", "<cmd>set hlsearch!<cr>")
 vim.keymap.set("c", "%%", '<c-r>=fnameescape(expand("%:h"))."/"<cr>')
 vim.keymap.set("n", "<leader>s", ":setlocal spell!<cr>")
 -------------------------- Plugins --------------------------------------
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 	-- Common
@@ -184,7 +200,18 @@ vim.g.netrw_winsize = 100
 vim.g.netrw_altv = 1
 vim.keymap.set({ "n", "i" }, "<c-g>", "<cmd>Vex<cr>")
 
-require("lazy").setup(plugins, {})
+-- Setup lazy.nvim
+require("lazy").setup(plugins, {
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
 require("snippy").setup({
 	mappings = {
 		is = {
