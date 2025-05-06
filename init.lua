504f3100128ce5c936cfd2cf18b8d8866590ec0e
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2025-05-07 01:42:48 +0300

    feat: editorconfig

--- init.lua
+++ init.lua
@@ -139,8 +139,8 @@ local plugins = {
 	"hrsh7th/cmp-nvim-lsp",
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-path",
-	'hrsh7th/cmp-cmdline',
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
 	"tpope/vim-commentary",
 	"tpope/vim-surround",
 	"tpope/vim-repeat",
@@ -233,16 +233,6 @@ vim.cmd([[
 
 -- LSP
 require("lspconfig").lua_ls.setup({
-	Lua = {
-		format = {
-			enabled = false,
-			defaultConfig = {
-				indent_style = "tab",
-				tab_width = "4",
-				max_line_length = "80",
-			},
-		},
-	},
 	on_init = function(client)
 		local path = client.workspace_folders[1].name
 		if
@@ -296,15 +286,15 @@ require("lspconfig").lua_ls.setup({
 -- http://ftp.vim.org/vim/runtime/spell/README.txt
 -- Set up nvim-cmp.
 
-local cmp = require('cmp')
 
+local cmp = require("cmp")
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
 			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			require("snippy").expand_snippet(args.body) -- For `snippy` users.
 			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 
@@ -320,20 +310,20 @@ cmp.setup({
 		-- documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
+		{ name = "nvim_lsp" },
 		-- { name = 'vsnip' }, -- For vsnip users.
 		-- { name = 'luasnip' }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
-		{ name = 'snippy' }, -- For snippy users.
+		{ name = "snippy" }, -- For snippy users.
 	}, {
-		{ name = 'buffer' },
+		{ name = "buffer" },
 	})
 })
 
@@ -349,27 +339,27 @@ cmp.setup({
  require("cmp_git").setup() ]] --
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
+cmp.setup.cmdline({ "/", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = 'buffer' }
+		{ name = "buffer" }
 	}
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = 'path' }
+		{ name = "path" }
 	}, {
-		{ name = 'cmdline' }
+		{ name = "cmdline" }
 	}),
 	matching = { disallow_symbol_nonprefix_matching = false }
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['lua_ls'].setup {
+require("lspconfig")["lua_ls"].setup {
 	capabilities = capabilities
 }
