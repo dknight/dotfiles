7ec4e056bfdbad642eab509cde65047dff719d34
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2026-04-12 01:04:48 +0300

    feat: asm ca65 plugin

--- nes.lua
+++ nes.lua
@@ -6,12 +6,25 @@ local emulator = "fceux"
 local compiler = "ca65"
 local linker = "cl65"
 local compile = string.format("<cmd>!%s %% -o %%<.o", compiler)
-local link = string.format("<cmd>!%s -t %s %%<.o -o %%<.nes", linker, type)
+local link = string.format(
+	"<cmd>!%s -t %s %%<.o -o %%<.nes",
+	linker,
+	type
+)
 local emulate = "<cmd>!" .. emulator .. " %<.nes"
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "asm" },
 	callback = function(args)
+		vim.bo.filetype = "asm_ca65"
+		vim.g.asmsyntax = "ca65"
+		vim.filetype.add({
+			extension = {
+				s = "asm",
+				asm = "asm",
+			},
+		})
+		vim.cmd("hi asmHexNumber guifg=#FFA500")
 		vim.keymap.set(
 			"n",
 			"<f5>",
