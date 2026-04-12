080a9b2dfd0978a3dab4f3832df449e1143c730d
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2026-04-13 00:58:44 +0300

    chore: setup ca65 syntax for nes

--- nes.lua
+++ nes.lua
@@ -1,44 +1,46 @@
 --------------------------------------------------------------------------
 -- NES/Famicom
 --------------------------------------------------------------------------
+vim.filetype.add({
+	extension = {
+		s = "asm_ca65",
+		asm = "asm_ca65",
+	},
+})
 local type = "nes"
 local emulator = "fceux"
 local compiler = "ca65"
 local linker = "cl65"
-local compile = string.format("<cmd>!%s %% -o %%<.o", compiler)
+local compile = string.format("<cmd>!%s %% -o %%<.o<cr>", compiler)
 local link = string.format(
-	"<cmd>!%s -t %s %%<.o -o %%<.nes",
+	"<cmd>!%s -t %s %%<.o -o %%<.nes<cr>",
 	linker,
 	type
 )
 local emulate = "<cmd>!" .. emulator .. " %<.nes"
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "asm" },
+	pattern = { "asm_ca65" },
 	callback = function(args)
-		vim.bo.filetype = "asm_ca65"
 		vim.g.asmsyntax = "ca65"
-		vim.filetype.add({
-			extension = {
-				s = "asm",
-				asm = "asm",
-			},
-		})
-		vim.cmd("hi asmHexNumber guifg=#FFA500")
+		vim.cmd("hi asm_ca65HexNumber ctermfg=12")
 		vim.keymap.set(
 			"n",
 			"<f5>",
-			"<cmd>w<cr>" .. compile .. "<cr>"
+			"<cmd>w<cr>" .. compile,
+			{ buffer = args.buf }
 		)
 		vim.keymap.set(
 			"n",
 			"<f6>",
-			"<cmd>w<cr>" .. link .. "<cr>"
+			"<cmd>w<cr>" .. link,
+			{ buffer = args.buf }
 		)
 		vim.keymap.set(
 			"n",
 			"<f10>",
-			compile .. "<cr>" .. link .. "<cr>" .. emulate .. "<cr>"
+			compile .. "<cr>" .. link .. "<cr>" .. emulate .. "<cr>",
+			{ buffer = args.buf }
 		)
 	end,
 })
